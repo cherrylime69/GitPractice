90963aa5d13500e461fecd16df7bd327c6ad4051
Linus <zerolecture0922@gmail.com>
2022-04-25 01:38:13 +0900

    BaseFragment 추가

--- app/src/main/java/com/example/gitpractice/BaseActivity.kt
+++ app/src/main/java/com/example/gitpractice/BaseActivity.kt
@@ -16,6 +16,7 @@ class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("안녕")
     }
 
 }
