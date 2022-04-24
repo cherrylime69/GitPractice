ea49bd3b7457e16af1867d489484869cf7c7bdbb
Linus <zerolecture0922@gmail.com>
2022-04-24 21:39:14 +0900

    BaseActivity 추가

---
+++ app/src/main/java/com/example/gitpractice/BaseActivity.kt
@@ -0,0 +1,21 @@
+package com.example.gitpractice
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.gitpractice.databinding.ActivityBaseBinding
+
+class BaseActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityBaseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
